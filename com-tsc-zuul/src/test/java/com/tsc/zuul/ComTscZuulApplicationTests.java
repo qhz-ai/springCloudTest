e7aff5fa7be2cf757360834196f29033d73cd41b
qhz-ai <2376484765@qq.com>
2021-06-23 20:52:51 +0800

    zuul网关

---
+++ com-tsc-zuul/src/test/java/com/tsc/zuul/ComTscZuulApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tsc.zuul;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComTscZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
